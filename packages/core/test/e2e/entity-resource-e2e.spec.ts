d12541d29ee5e7090831846eaed87e7afc390009
Eray Hanoğlu <e.hanoglu@panates.com>
2022-09-23 10:05:31 +0300

    Implemented Metadata requesting

--- packages/core/test/e2e/entity-resource-e2e.spec.ts
+++ packages/core/test/e2e/entity-resource-e2e.spec.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
-import request from 'supertest';
+import { apiExpect, opraTest, OpraTester } from '@opra/testing';
 import { OpraExpressAdapter, OpraService } from '../../src/index.js';
 import { createTestService } from '../_support/test-app/create-service.js';
 
@@ -8,25 +8,56 @@ describe('Entity Resource Operations', function () {
 
   let service: OpraService;
   let app;
+  let api: OpraTester;
 
   beforeAll(async () => {
     service = await createTestService();
     app = express();
     await OpraExpressAdapter.init(app, service);
+    api = opraTest(app);
   });
 
-  it('Should request "get" operations', async () => {
-    const resp = await request(app).get('/Customers@1');
-    expect(resp.status).toStrictEqual(200);
-    expect(resp.body).toBeDefined();
-    expect(resp.body).toMatchObject({
-      "@opra:metadata": "/$metadata",
-      "version": "1.0",
-      "info": {
-        "title": "TestApi",
-        "version": "v1"
-      }
+  describe('"get" requests', function () {
+
+    it('Should return object', async () => {
+      const resp = await api.entity('Customers')
+          .get('1').send();
+      apiExpect(resp)
+          .toSuccess()
+          .toReturnObject(obj => {
+            obj.toMatch({
+              "@opra:metadata": "/$metadata/types/Customer",
+              "id": 1
+            })
+          })
     })
   })
+
+  describe('"search" search', function () {
+
+    it('Should return list object', async () => {
+      const resp = await api.entity('Customers')
+          .search().send();
+      apiExpect(resp)
+          .toSuccess()
+          .toReturnList(list => {
+            list.toHaveMinItems(1);
+          })
+    })
+
+    it('Should apply filter', async () => {
+      const resp = await api.entity('Customers')
+          .search()
+          .filter('countryCode="US"')
+          .send();
+      apiExpect(resp)
+          .toSuccess()
+          .toReturnList(list => {
+            list.toHaveMinItems(1);
+            list.toBeFilteredBy('countryCode="US"');
+          })
+    })
+  });
+
 
 });
