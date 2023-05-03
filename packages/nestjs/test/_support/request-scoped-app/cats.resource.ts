54fcdcc50ff29f50d2c3e02514014726d1ceeddb
Eray Hanoğlu <e.hanoglu@panates.com>
2023-05-03 10:07:48 +0300

    Added mongodb support Improvements Bug fixes

--- packages/nestjs/test/_support/request-scoped-app/cats.resource.ts
+++ packages/nestjs/test/_support/request-scoped-app/cats.resource.ts
@@ -4,13 +4,16 @@ import { Cat } from './cat.dto.js';
 import { CatsService } from './cats.service.js';
 import { Interceptor } from './logging.interceptor.js';
 import { Guard } from './request-scoped.guard.js';
+import { UsersService } from './users.service.js';
 
 @Collection(Cat, {primaryKey: 'id'})
 export class CatsResource {
   static COUNTER = 0;
 
+  // noinspection JSUnusedLocalSymbols
   constructor(
-      private readonly catsService: CatsService
+      private readonly catsService: CatsService,
+      private readonly usersService: UsersService
   ) {
     CatsResource.COUNTER++;
   }
