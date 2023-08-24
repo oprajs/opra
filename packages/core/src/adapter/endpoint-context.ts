a84c8eacee58b6e22b2460ef391bbd2043a3d72e
Eray Hanoğlu <e.hanoglu@panates.com>
2023-08-24 09:52:24 +0300

    Renamed "requestScope" to "space"

--- packages/core/src/adapter/endpoint-context.ts
+++ packages/core/src/adapter/endpoint-context.ts
@@ -2,21 +2,23 @@ import type { ExecutionContext } from './execution-context';
 import type { Request } from './request';
 import type { Response } from './response';
 
-export interface EndpointContext<TRequest extends Request = Request, TResponse extends Response = Response> extends ExecutionContext {
-  request: TRequest;
-  response: TResponse;
-  requestScope: Record<string | number | symbol, any>;
+export interface EndpointContext<TSpace extends object = Record<string | number | symbol, any>> extends ExecutionContext {
+  request: Request;
+  response: Response;
+  space: TSpace;
 }
 
 export namespace EndpointContext {
-  export function from<TRequest extends Request = Request, TResponse extends Response = Response>(
-      executionContext: ExecutionContext, request: TRequest, response: TResponse
+  export function from(
+      executionContext: ExecutionContext,
+      request: Request,
+      response: Response
   ): EndpointContext {
     const out = {
       request,
       response,
-      requestScope: {}
-    } as EndpointContext<TRequest, TResponse>;
+      space: {}
+    } as EndpointContext;
     Object.setPrototypeOf(out, executionContext);
     return out;
   }
