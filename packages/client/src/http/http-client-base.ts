94562ab467528af341d2149e48ee6ce7eaed4606
Eray Hanoğlu <e.hanoglu@panates.com>
2024-07-16 18:01:56 +0300

    Fixed: Do not merge serviceUrl and request path well in browsers

--- packages/client/src/http/http-client-base.ts
+++ packages/client/src/http/http-client-base.ts
@@ -4,6 +4,8 @@ import { kBackend } from '../constants.js';
 import { HttpBackend } from './http-backend.js';
 import { HttpRequestObservable } from './http-request-observable.js';
 
+const SPLIT_BACKSLASH_PATTERN = /^(\/*)(.+)/;
+
 /**
  *
  * @namespace OpraClientBase
@@ -69,7 +71,8 @@ export abstract class HttpClientBase<TRequestOptions = {}, TResponseExt = {}> {
   }
 
   request<TBody = any>(path: string, options?: OpraClientBase.RequestOptions) {
-    path = /^(\/*)(.+)/.exec(path)?.[2] || '';
+    /** Remove leading backslashes */
+    path = SPLIT_BACKSLASH_PATTERN.exec(path)?.[2] || '';
     const observable = new HttpRequestObservable<TBody, TBody, TRequestOptions, TResponseExt>(this[kBackend], {
       ...options,
       method: options?.method || 'GET',
