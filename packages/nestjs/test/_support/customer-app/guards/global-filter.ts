6bc5bc5c8c82aee403c8cf0c636278511250c02f
Eray Hanoğlu <e.hanoglu@panates.com>
2024-07-25 01:00:57 +0300

    chore: Lint fixes

--- packages/nestjs/test/_support/customer-app/guards/global-filter.ts
+++ packages/nestjs/test/_support/customer-app/guards/global-filter.ts
@@ -1,11 +1,11 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import { Catch, HttpException } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch(HttpException)
 export class GlobalExceptionFilter extends BaseExceptionFilter {
   static counter = 0;
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException) {
     GlobalExceptionFilter.counter++;
     throw exception;
   }
