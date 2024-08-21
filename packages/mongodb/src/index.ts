65cee926202ff674f41f4e47784d3333903f7a7a
Eray Hanoğlu <e.hanoglu@panates.com>
2024-08-21 09:56:51 +0300

    Fixed compatibility for "Node16" and "NodeNext" moduleResolution options

--- packages/mongodb/src/index.ts
+++ packages/mongodb/src/index.ts
@@ -1,5 +1,6 @@
 export * from './mongo-adapter.js';
 export * from './mongo-collection-service.js';
+export * from './mongo-entity-service.js';
 export * from './mongo-nested-service.js';
 export * from './mongo-service.js';
 export * from './mongo-singleton-service.js';
