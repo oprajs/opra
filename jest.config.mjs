f437be491144a98a4a507dcae26c0fe3f9f92d9c
Eray Hanoğlu <e.hanoglu@panates.com>
2024-09-23 12:16:14 +0300

    chore: Updated versions

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,24 +3,16 @@ import base from './jest.config.base.cjs';
 export default {
   ...base,
   verbose: true,
+  testSequencer: '<rootDir>/support/test/jest-customsequencer.cjs',
+  projects: ['<rootDir>/packages/*/jest.config.mjs'],
+  reporters: ['default'],
+  coverageReporters: ['lcov'],
   coverageDirectory: '<rootDir>/reports/',
   coveragePathIgnorePatterns: [
     '<rootDir>/build/',
     '<rootDir>/dist/',
+    '<rootDir>/test/',
     '/node_modules/',
     '_support',
   ],
-  testSequencer: '<rootDir>/support/test/jest-customsequencer.cjs',
-  projects: ['<rootDir>/packages/*/jest.config.mjs'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'reports',
-        outputName: 'jest-junit.xml',
-      },
-    ],
-  ],
-  coverageReporters: ['lcov', 'json-summary'],
 };
