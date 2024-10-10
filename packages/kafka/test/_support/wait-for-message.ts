da677673dfa012c7670034420d252784a19f68bd
Eray Hanoğlu <e.hanoglu@panates.com>
2024-10-10 17:17:16 +0300

    chore: Added logging for kafka tests

--- packages/kafka/test/_support/wait-for-message.ts
+++ packages/kafka/test/_support/wait-for-message.ts
@@ -1,15 +1,33 @@
 import { KafkaAdapter, KafkaContext } from '@opra/kafka';
 
+const waitList = new Set();
+
 export async function waitForMessage(adapter: KafkaAdapter, oprname: string, key: any): Promise<KafkaContext> {
   return new Promise((resolve, reject) => {
+    const waitKey = oprname + ':' + key;
+    waitList.add(waitKey);
     const onMessage = async (_ctx: KafkaContext) => {
-      if (_ctx.operation?.name === oprname && _ctx.key === key) {
-        adapter.removeListener('error', onError);
-        adapter.removeListener('after-execute', onMessage);
-        resolve(_ctx);
+      if (_ctx.operation?.name === oprname) {
+        if (_ctx.key === key) {
+          adapter.removeListener('error', onError);
+          adapter.removeListener('after-execute', onMessage);
+          waitList.delete(waitKey);
+          resolve(_ctx);
+        } else {
+          if (waitList.has(waitKey)) return;
+          // eslint-disable-next-line no-console
+          console.warn(`Warning: Waiting message with "${key}" key but god message with "${_ctx.key}"`);
+        }
+      } else {
+        if (waitList.has(waitKey)) return;
+        // eslint-disable-next-line no-console
+        console.warn(
+          `Warning: Waiting message for "${oprname}" operation but god message for "${_ctx.operation?.name}"`,
+        );
       }
     };
     const onError = (e: any) => {
+      waitList.delete(waitKey);
       adapter.removeListener('after-execute', onMessage);
       reject(e);
     };
