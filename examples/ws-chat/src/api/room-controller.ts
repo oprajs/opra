146ddb40f84212d757e190d48bdd99c944e3742f
Eray Hanoğlu <e.hanoglu@panates.com>
2026-04-17 12:37:52 +0300

    refactor: remove unused parameters and redundant code in room-controller

--- examples/ws-chat/src/api/room-controller.ts
+++ examples/ws-chat/src/api/room-controller.ts
@@ -14,14 +14,11 @@ export class RoomController {
     ctx: SocketioContext,
     @WsParam()
     roomName: string,
-    @WsParam()
-    message: string,
   ) {
     const room = this.app.roomService.rooms.get(roomName);
     if (!room) throw new Error(`Room ${roomName} does not exist`);
     if (!room.users.includes(ctx.socket.data.user))
       room.users.push(ctx.socket.data.user);
-    ctx.__controller;
     return true;
   }
 }
